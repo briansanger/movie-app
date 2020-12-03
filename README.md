bfe3c86bd0f637917ef15e99673f3fd3c9995678
Brian Sanger <sanger.brian@gmail.com>
2020-12-02 21:11:02 -0600

    Functions

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # movie-app
  
+https://medium.com/javascript-in-plain-english/implement-movie-app-with-vue-vuetify-axios-open-movie-database-api-d12290318cf9
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import vuetify from '@/plugins/vuetify';
+import App from './App.vue';
+import { store } from './store';
+import router from './router';
+import axios from 'axios';
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+axios.defaults.baseURL = 'http://www.omdbapi.com/?apikey=b76b385c&page=1&type=movie&Content-Type=application/json';
+
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app');
